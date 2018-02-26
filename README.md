a23c92622d97024b28b036d2bbc372d11def1f77
Uzeda <guilherme.uzeda@santodigital.com.br>
2018-02-26 15:55:33 -0300

    readme

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
-# MS-Celeb-1M-helper
+# MS-Celeb-1M Helpers
+
+## Some helpers to the MS-Celeb-1M database
+
+## download.py:
+A download script for the MS-Celeb-1M's croped DB. It will downalod all the db's parts parallelized in 10 threads
+
+    optional arguments:
+    -h, --help         show this help message and exit
+    -d DIR, --dir DIR  Directory to save the data
+
+
+## extract.py:
+TODO: README.md
--- download.py
+++ download.py
@@ -3,22 +3,14 @@ import urllib2
 import os
 
 from threading import Thread
+import argparse
+
 
 import logging
 
 logging.basicConfig(level=logging.DEBUG,
                     format='[%(levelname)s] (%(threadName)-10s) %(message)s',
                     )
-
-dest_path = "data/"
-
-response = urllib2.urlopen(
-    'https://msceleb.blob.core.windows.net/ms-celeb-v1-cropped-split?restype=container&comp=list')
-html = response.read()
-
-soup = BeautifulSoup(html, 'lxml')
-
-urls=  soup.find_all("url")
 
 
 class Th(Thread):
@@ -40,7 +32,7 @@ class Th(Thread):
         logging.debug( "Downloading: %s Bytes: %s" % ( self.fname , file_size))
         file_size_dl = 0
         block_sz = 8192
-        
+        p = 500
         while True:
             buffer = u.read(block_sz)
             if not buffer:
@@ -50,14 +42,38 @@ class Th(Thread):
             f.write(buffer)
             status = r"%10d  [%3.2f%%]" % (file_size_dl, file_size_dl * 100. / file_size)
             status = status + chr(8)*(len(status)+1)
-            logging.debug(status) 
+            if p == 500:
+                logging.debug(status)   
+                p=0
+            p += 1    
 
         f.close()
 
-thrs = []
-for thread_number in range (len(urls)):
+def main(args):
 
-    a = Th(thread_number, str(urls[thread_number].string), dest_path)
-    a.start()
-    thrs.append(a)
 
+
+    dest_path = args.dir
+
+    response = urllib2.urlopen(
+        'https://msceleb.blob.core.windows.net/ms-celeb-v1-cropped-split?restype=container&comp=list')
+    html = response.read()
+
+    soup = BeautifulSoup(html, 'lxml')
+
+    urls=  soup.find_all("url")
+    thrs = []
+    for thread_number in range (len(urls)):
+
+        a = Th(thread_number, str(urls[thread_number].string), dest_path)
+        a.start()
+        thrs.append(a)
+
+if __name__ == "__main__":
+    cwd = os.getcwd()
+    parser = argparse.ArgumentParser(description="A download script for the MS-Celeb-1M's croped DB")
+    parser.add_argument("-d", "--dir", help="Directory to save the data",
+                        default=os.path.join(cwd, "data"))
+    args = parser.parse_args()
+
+    main(args)
